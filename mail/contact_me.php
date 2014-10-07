d91ea29b90e418f75ff7ea64bb1f6b4bf775cde3
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-10-07 01:57:07 -0400

    changed mail form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,7 +20,7 @@ $message = $_POST['message'];
 $to = 'filippos.lympe@gmail.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; 
+$headers = "From: $to\n"; 
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
